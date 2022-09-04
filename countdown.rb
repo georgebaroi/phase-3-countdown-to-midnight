9b15d53eb6a3cd6cdf3a2780f71f5bfbcbbd6655
George Baroi <baroi.george@gmail.com>
2022-09-03 21:19:16 -0400

    commit tree f41e2692556a95b8f6ddd49aa3b63ae905061e1a

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    x = 10
+    while x > 0 
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        if x == 0 
+            return "HAPPY NEW YEAR!"
+        end
+    end
 end
+
+def countdown_with_sleep(x)
+    x = 10 
+    while x > 0 
+        puts "#{x}"
+        x-= 1 && sleep(1)
+        if x == 0 
+            return "HAPPY NEW YEAR!"
+        end
+    end
+ end
+
